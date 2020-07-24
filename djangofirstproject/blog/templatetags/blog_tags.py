299c226af14ec7906f9dbc0f59af3151252cdc2b
faran <farantgh@gmail.com>
2020-07-25 01:55:50 +0430

    year archive added

--- djangofirstproject/blog/templatetags/blog_tags.py
+++ djangofirstproject/blog/templatetags/blog_tags.py
@@ -29,30 +29,11 @@ def markdown_format(text):
     return mark_safe(markdown.markdown(text))
 
 
-@register.simple_tag
-def xrange():
-    first_post = Post.published.all().order_by('publish')[:1]
-    for p in first_post:
-        first_post_year = p.publish.year
-
-    last_post = Post.published.all().order_by('-publish')[:1]
-    for p in last_post:
-        last_post_year = p.publish.year
-    holder = []
-    for n in range(first_post_year, last_post_year+1):
-        holder.append(n)
-    return holder
-
-@register.simple_tag
-def first_year():
-    first_post = Post.published.all().order_by('publish')[:1]
-    for p in first_post:
-        first_post_year = p.publish.year
-    return first_post_year
-
-@register.simple_tag
-def last_year():
-    last_post = Post.published.all().order_by('-publish')[:1]
-    for p in last_post:
-        last_post_year = p.publish.year
-    return last_post_year
+@register.inclusion_tag('blog/post/archive.html')
+def archive():
+	list_years = []
+	posts = Post.published.all()
+	for post in posts:
+		list_years.append(post.publish.year)
+	list_years = list(set(list_years))
+	return {'posts': list_years}
